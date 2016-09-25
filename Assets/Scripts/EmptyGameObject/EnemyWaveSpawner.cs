d28805400e4f6b38688198ef68f7f9bcdc73645e
DarthCorbi <123.gaming.12@gmail.com>
2016-09-25 23:53:46 +0200

    Balancing #1
    Changes:
    
    Script:
    EnemyWaveSpawner
    changed spawn system to array
    changed amount of waves to 20

--- Assets/Scripts/EmptyGameObject/EnemyWaveSpawner.cs
+++ Assets/Scripts/EmptyGameObject/EnemyWaveSpawner.cs
@@ -16,250 +16,71 @@ public class EnemyWaveSpawner : MonoBehaviour {
     public float timeBetweenWaves = 5f;
     private float countdown = 2f;
 
-	[Header("Wellen")]
-	public WaveManager Wave_1;
-	public WaveManager Wave_2;
-	public WaveManager Wave_3;
-	public WaveManager Wave_4;
-	public WaveManager Wave_5;
-	public WaveManager Wave_6;
-	public WaveManager Wave_7;
-	public WaveManager Wave_8;
-	public WaveManager Wave_9;
-	public WaveManager Wave_10;
+    public WaveManager[] Waves;
+
+    GameObject[] enemies;
 
     private int waveNumber = 0;
 
+    void Start()
+    {
+        StartCoroutine(SpawnWave());
+    }
+
     void Update()
     {
+        enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        /***
         if (countdown <= 0f)
         {
             StartCoroutine(SpawnWave());
             countdown = timeBetweenWaves;
         }
-		GameObject[] enemies = GameObject.FindGameObjectsWithTag (enemyTag);
-			
-		if (enemies.Length == 0)
-		{
-        	countdown -= Time.deltaTime;
-		}
+
+
+        if (enemies.Length == 0)
+        {
+            countdown -= Time.deltaTime;
+        }
+        ***/
     }
 
     IEnumerator SpawnWave()
     {
 
-        waveNumber++;
-        waveCounterText.text = waveNumber.ToString();
+        foreach (WaveManager wave in Waves)
+        {
+            waveNumber++;
+            waveCounterText.text = waveNumber.ToString();
 
-		if (waveNumber == 1) {			
-		
-        	for (int i = 0; i < Wave_1.anzahlEnemies; i++)
-        	{
-            	SpawnEnemy();
+            for (int i = 0; i < wave.anzahlEnemies; i++)
+            {
+                SpawnEnemy();
 
-            	yield return new WaitForSeconds(0.5f);
+                yield return new WaitForSeconds(0.5f);
             }
-            for (int i = 0; i < Wave_1.anzahlEnemies2; i++)
+            for (int i = 0; i < wave.anzahlEnemies2; i++)
             {
                 SpawnEnemy2();
 
                 yield return new WaitForSeconds(0.5f);
             }
 
-            for (int i = 0; i < Wave_1.anzahlEnemies3; i++)
+            for (int i = 0; i < wave.anzahlEnemies3; i++)
             {
                 SpawnEnemy3();
 
                 yield return new WaitForSeconds(0.5f);
             }
+
+            
+            while (enemies.Length > 0)
+            {
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(timeBetweenWaves);
         }
-		else if (waveNumber == 2) {			
-
-			for (int i = 0; i < Wave_2.anzahlEnemies; i++)
-			{
-				SpawnEnemy();
-
-				yield return new WaitForSeconds(0.5f);
-			}
-            for (int i = 0; i < Wave_2.anzahlEnemies2; i++)
-            {
-                SpawnEnemy2();
-
-                yield return new WaitForSeconds(0.5f);
-            }
-
-            for (int i = 0; i < Wave_2.anzahlEnemies3; i++)
-            {
-                SpawnEnemy3();
-
-                yield return new WaitForSeconds(0.5f);
-            }
-        }
-		else if (waveNumber == 3) {			
-
-			for (int i = 0; i < Wave_3.anzahlEnemies; i++)
-			{
-				SpawnEnemy();
-
-				yield return new WaitForSeconds(0.5f);
-			}
-            for (int i = 0; i < Wave_3.anzahlEnemies2; i++)
-            {
-                SpawnEnemy2();
-
-                yield return new WaitForSeconds(0.5f);
-            }
-            for (int i = 0; i < Wave_3.anzahlEnemies3; i++)
-            {
-                SpawnEnemy3();
-
-                yield return new WaitForSeconds(0.5f);
-            }
-        }
-		else if (waveNumber == 4) {			
-
-			for (int i = 0; i < Wave_4.anzahlEnemies; i++)
-			{
-				SpawnEnemy();
-
-				yield return new WaitForSeconds(0.5f);
-			}
-            for (int i = 0; i < Wave_4.anzahlEnemies2; i++)
-            {
-                SpawnEnemy2();
-
-                yield return new WaitForSeconds(0.5f);
-            }
-
-            for (int i = 0; i < Wave_4.anzahlEnemies3; i++)
-            {
-                SpawnEnemy3();
-
-                yield return new WaitForSeconds(0.5f);
-            }
-        }
-		else if (waveNumber == 5) {			
-
-			for (int i = 0; i < Wave_5.anzahlEnemies; i++) {
-				SpawnEnemy ();
-
-				yield return new WaitForSeconds (0.5f);
-			}
-            for (int i = 0; i < Wave_5.anzahlEnemies2; i++)
-            {
-                SpawnEnemy2();
-
-                yield return new WaitForSeconds(0.5f);
-            }
-
-            for (int i = 0; i < Wave_5.anzahlEnemies3; i++)
-            {
-                SpawnEnemy3();
-
-                yield return new WaitForSeconds(0.5f);
-            }
-        } else if (waveNumber == 6) {			
-
-			for (int i = 0; i < Wave_6.anzahlEnemies; i++) {
-				SpawnEnemy ();
-
-				yield return new WaitForSeconds (0.5f);
-			}
-            for (int i = 0; i < Wave_6.anzahlEnemies2; i++)
-            {
-                SpawnEnemy2();
-
-                yield return new WaitForSeconds(0.5f);
-            }
-
-            for (int i = 0; i < Wave_6.anzahlEnemies3; i++)
-            {
-                SpawnEnemy3();
-
-                yield return new WaitForSeconds(0.5f);
-            }
-        } else if (waveNumber == 7) {			
-
-			for (int i = 0; i < Wave_7.anzahlEnemies; i++) {
-				SpawnEnemy ();
-
-				yield return new WaitForSeconds (0.5f);
-			}
-            for (int i = 0; i < Wave_7.anzahlEnemies2; i++)
-            {
-                SpawnEnemy2();
-
-                yield return new WaitForSeconds(0.5f);
-            }
-
-            for (int i = 0; i < Wave_7.anzahlEnemies3; i++)
-            {
-                SpawnEnemy3();
-
-                yield return new WaitForSeconds(0.5f);
-            }
-        } else if (waveNumber == 8) {			
-
-			for (int i = 0; i < Wave_8.anzahlEnemies; i++) {
-				SpawnEnemy ();
-
-				yield return new WaitForSeconds (0.5f);
-			}
-            for (int i = 0; i < Wave_8.anzahlEnemies2; i++)
-            {
-                SpawnEnemy2();
-
-                yield return new WaitForSeconds(0.5f);
-            }
-
-            for (int i = 0; i < Wave_8.anzahlEnemies3; i++)
-            {
-                SpawnEnemy3();
-
-                yield return new WaitForSeconds(0.5f);
-            }
-        } else if (waveNumber == 9) {			
-
-			for (int i = 0; i < Wave_9.anzahlEnemies; i++) {
-				SpawnEnemy ();
-
-				yield return new WaitForSeconds (0.5f);
-			}
-            for (int i = 0; i < Wave_9.anzahlEnemies2; i++)
-            {
-                SpawnEnemy2();
-
-                yield return new WaitForSeconds(0.5f);
-            }
-
-            for (int i = 0; i < Wave_9.anzahlEnemies3; i++)
-            {
-                SpawnEnemy3();
-
-                yield return new WaitForSeconds(0.5f);
-            }
-        } else if (waveNumber == 10) {			
-
-			for (int i = 0; i < Wave_10.anzahlEnemies; i++) {
-				SpawnEnemy ();
-
-				yield return new WaitForSeconds (0.5f);
-			}
-            for (int i = 0; i < Wave_10.anzahlEnemies2; i++)
-            {
-                SpawnEnemy2();
-
-                yield return new WaitForSeconds(0.5f);
-            }
-            for (int i = 0; i < Wave_10.anzahlEnemies3; i++)
-            {
-                SpawnEnemy3();
-
-                yield return new WaitForSeconds(0.5f);
-            }
-        } else {
-			Debug.Log ("Alle Wellen besiegt!!!11!!!1!");
-		}
     }
 	
 
